3bcfd79aad96d56f06123cfa79cfc61279a1f78f
harpagun333a <matorz@student.agh.edu.pl>
2020-01-06 22:44:26 +0100

    DONE

--- Cwiczenie_01_Choinka/Cwiczenie_01_Choinka/Program.cs
+++ Cwiczenie_01_Choinka/Cwiczenie_01_Choinka/Program.cs
@@ -18,13 +18,14 @@ namespace Cwiczenie_01_Choinka
         int branches = 15;
         Random rnd = new Random();
         int rand;
+        int line = 0;
+        int counter = 0;
+ 
         public void rysuj()
         {
-            //Console.Clear();
             while (true)
             {
                 System.Threading.Thread.Sleep(500);
-                //Console.Clear();
                 Console.SetCursorPosition(0, 2);
                 for (int i = 0; i <= branches; i++)
                 {
@@ -64,21 +65,65 @@ namespace Cwiczenie_01_Choinka
                 }
                 Console.WriteLine();
 
-                //System.Threading.Thread.Sleep(500);
-                //Console.Clear();
-                //for (int i = 0; i <= branches; i++)
-                //{
-                //    for (int j = i; j < branches; j++)
-                //    {
-                //        Console.Write(' ');
-                //    }
-                //    for (int j = i * 2 - 1; j > 0; j--)
-                //    {
-                //        Console.Write('^');
-                //    }
-                //    Console.WriteLine();
-                //}
+                counter++;
+                if (counter == 3)
+                {
+                    counter = 0;
+                    line++;
+                    if (line == text.Length)
+                    {
+                        line = 0;
+                    }
+                }
+
+                Console.SetCursorPosition(29, 11);
+                Console.Write("                                          ");
+                Console.SetCursorPosition(29, 11);
+                for (int i = 0; i < text[line].Length+2; i++)
+                {
+                    Console.Write('_');
+                }
+                Console.SetCursorPosition(28, 12);
+                Console.Write("                                          ");
+                Console.SetCursorPosition(28, 12);
+                Console.Write("< " + text[line] + " >");
+                Console.SetCursorPosition(29, 13);
+                Console.Write("                                          ");
+                Console.SetCursorPosition(29, 13);
+                for (int i = 0; i < text[line].Length+2; i++)
+                {
+                    Console.Write('-');
+                }
+                Console.SetCursorPosition(33, 14);
+                Console.Write("\\   ^__ ^");
+                Console.SetCursorPosition(33, 15);
+                Console.Write(" \\  (oo)\\_______");
+                Console.SetCursorPosition(33, 16);
+                Console.Write("    (__)\\       )\\/\\");
+                Console.SetCursorPosition(33, 17);
+                Console.Write("        || ----w |");
+                Console.SetCursorPosition(33, 18);
+                Console.Write("        ||     ||");
+
             }
         }
+
+        string[] text =
+        {
+            "...",
+            "Dzisiaj w Betlejem, Dzisiaj w Betlejem",
+            "Wesoła Nowina!",
+            "Że Panna czysta, że Panna czysta",
+            "Porodziła Syna.",
+            "Chrystus się rodzi,",
+            "Nas oswobodzi,",
+            "Anieli grają,",
+            "Króle witają,",
+            "Pasterze śpiewają,",
+            "Bydlęta klękają,",
+            "Cuda, cuda ogłaszają!",
+            "..."
+        };
     }
+
 }
